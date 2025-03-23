850e149ace417c99dca6f0d39c2f7d8530937790
Olyamh <olyamh234@yahoo.com>
2025-03-23 20:31:57 +0200

    add hw

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,12 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+
+export const generateContacts = async (number) => {
+    const contactList = await readContacts();
+    const newContactList = Array(number).fill(0).map(createFakeContact);
+    await writeContacts([...contactList, ...newContactList]);
+};
 
 generateContacts(5);
